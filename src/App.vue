d664aeced4ad6a9e9f8c893eb43cc7ad15608e95
atbeesley <alexander.beesley@gmail.com>
2020-05-22 09:05:49 +0100

    input field working correctly

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>Todos los todos</h1>
+    <input type="text" v-model="todoName" @keyup.enter="addTodo">
       <ul>
         <li v-for="todo of todos" :key="todo.id">{{todo.name}}</li>
       </ul>
@@ -10,19 +11,31 @@
 <script>
 import axios from 'axios';
 
+const baseURL = "http://localhost:3000/todos";
+
 export default {
   name: "app",
   data() {
     return {
-      todos: []
+      todos: [],
+      todoName: ''
     };
   },
   async created(){
     try {
-      const res = await axios.get('http://localhost:3000/todos');
+      const res = await axios.get(baseURL);
       this.todos = res.data;
     } catch(e){
-      console.log(e);
+      console.error(e);
+    }
+  },
+  methods: {
+    async addTodo(){
+      const res = await axios.post(baseURL, { name: this.todoName});
+
+      this.todos = [...this.todos, res.data];
+
+      this.todoName = '';
     }
   }
 };
